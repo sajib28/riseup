d7bdf1ac82fddafadf8a460ddcea88b4c08d2e8d
sajib28 <sajibcse28@gmail.com>
2019-10-10 19:06:58 +0600

    Webpack added

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,25 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
         }
       },
       {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
+  plugins: [htmlWebpackPlugin]
 };
